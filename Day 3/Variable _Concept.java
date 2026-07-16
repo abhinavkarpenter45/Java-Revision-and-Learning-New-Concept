d50f7003d462d65558c5df2660f1b451f66637d1
Abhinav Karpenter <abhinavcarpenter0@gmail.com>
2026-07-16 22:56:01 +0530

    Today I learn Concept of Variable and Java Lifecycle

---
+++ Day 3/Variable _Concept.java
@@ -0,0 +1,75 @@
+/*
+                    ========================================
+                                JAVA REVISION - DAY 2
+                    ========================================
+*/
+
+/*
+Topic = What is Variable ?
+Variable : it is a Memory Location which can Store Value.
+
+Example:
+int a = 20;
+int -> it is a datatype
+a -> variable name
+= -> sign to assign value
+20 -> it is actual value
+
+--------------------------------------------------------------------------------------------------------
+
+Datatypes in Java:
+Type   -> Size    -> Example
+int    -> 4byte   -> 20
+float  -> 4byte   -> 20.3f, 20.98f
+char   -> 2byte   -> "a", "Hello"
+double -> 8byte   -> 3.141592
+boalean -> 1bit    -> true & false
+long   -> 8byte   ->100000L
+
+--------------------------------------------------------------------------------------------------------
+
+Primitive Data Types:
+--it is a built in datatype
+--they are store value directly
+
+Features:
+    fixed size
+    fast
+    store in stack memory
+    do not store NULL values
+
+Examples: short, long, int, float, double, char, boalean
+
+--------------------------------------------------------------------------------------------------------
+
+Non-Primitive Datatypes;
+--it is reference type 
+--it store address/location of object
+--do not store directly values
+--user define + built-in datatypes
+
+Features:
+    not fix size
+    store NULL values
+    use Heap memory
+
+Example: Array, string, object, class, interface, Enum.
+
+--------------------------------------------------------------------------------------------------------
+
+types of variables:
+
+1.local variable
+    Declared inside a method, constructor, or block
+    it can be used only within that method, constructor, or block
+
+2.Instance variable
+    Declared inside a class but outside a method, constructor or block
+    each object has the it own separate copy of a instance variable
+
+3.Static variable
+    Declared with static keyword inside a class but outside methods
+    only one copy exists and it is shared by all object of the classs
+
+    --------------------------------------------------------------------------------------------------------
+*/
