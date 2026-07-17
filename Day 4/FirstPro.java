caac5e5e0f2f04defc5ea752c9387588ea55fac2
Abhinav Karpenter <abhinavcarpenter0@gmail.com>
2026-07-17 21:41:15 +0530

    Day 4: Java Rules, First Program & Primitive Data Types

---
+++ Day 4/FirstPro.java
@@ -0,0 +1,14 @@
+//Questions: Print Hello World
+
+public class FirstPro {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+//public -> An access modifier that makes a class or method accessible from anywhere in the program.
+//static -> A keyword that indicates a member belongs to the class rather than an object, so it can be accessed without creating an instance of the class.
+//void -> A return type that specifies the method does not return any value.
+//main -> The entry point of a Java application. The JVM starts program execution from the main() method.
+//String[] args -> A parameter that stores command-line arguments as an array of String objects passed to the program when it starts.
+
