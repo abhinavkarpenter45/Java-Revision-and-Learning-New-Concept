21d2f6ea72b879964392b3a3c0d2f5465ad0fb61
Abhinav Karpenter <abhinavcarpenter0@gmail.com>
2026-07-17 21:49:27 +0530

    Day 4: Java Rules, First Program & Primitive Data Types

---
+++ Day 4/Primitive_DataType.java
@@ -0,0 +1,30 @@
+public class Primitive_DataType {
+    public static void main(String[] args) {
+        
+    // Integer Types
+        byte byteValue = 100;
+        short shortValue = 30000;
+        int intValue = 100000;
+        long longValue = 10000000000L;
+
+        // Decimal Types
+        float floatValue = 99.99F;
+        double doubleValue = 12345.6789;
+
+        // Character Type
+        char charValue = 'A';
+
+        // Boolean Type
+        boolean booleanValue = true;
+
+        // Print Values
+        System.out.println("Byte: " + byteValue);
+        System.out.println("Short: " + shortValue);
+        System.out.println("Int: " + intValue);
+        System.out.println("Long: " + longValue);
+        System.out.println("Float: " + floatValue);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Char: " + charValue);
+        System.out.println("Boolean: " + booleanValue);
+    }
+}
