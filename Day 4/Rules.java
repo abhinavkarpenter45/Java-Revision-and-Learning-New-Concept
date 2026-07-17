710914f33145aa8047bd5e94fc6939e95274d9c9
Abhinav Karpenter <abhinavcarpenter0@gmail.com>
2026-07-17 21:48:35 +0530

    Day 4: Java Rules, First Program & Primitive Data Types

---
+++ Day 4/Rules.java
@@ -0,0 +1,15 @@
+//Important Rules:
+// 1.Every Java program must contain at least one class.
+// 2.If a class is public, the file name must match the class name.
+// 3.Every Java application starts execution from the main() method.
+// 4.Java is case-sensitive (System ≠ system).
+// 5.Every statement must end with a semicolon (;), except blocks and declarations that don't require one
+// 6.Curly braces {} are used to define the beginning and end of a class, method, loop, or conditional block.
+// 7.Variables must be declared before they are used.
+// 8.Java keywords (such as class, public, static, int) cannot be used as identifiers.
+// 9.Identifiers (class, method, and variable names) must follow Java naming rules.
+// 10.Comments (//, /* */) are ignored by the compiler and do not affect program execution.
+// 11.Java follows the Object-Oriented Programming (OOP) paradigm, so all code is written inside classes.
+// 12.The source file must have the .java extension.
+// 13.Java source code is first compiled into bytecode (.class file) by the Java Compiler (javac).
+// 14.The Java Virtual Machine (JVM) executes the compiled bytecode.
