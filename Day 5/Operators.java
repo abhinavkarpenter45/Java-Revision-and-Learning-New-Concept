717062a6399273cb4b13e8971e6400383e4ce748
Abhinav Karpenter <abhinavcarpenter0@gmail.com>
2026-07-19 23:18:02 +0530

    Day 5:Learn Concept of User Input and Operators

---
+++ Day 5/Operators.java
@@ -0,0 +1,11 @@
+// What is Operators ?
+// Operators is Symbols it is used to perfrom any operations like Addition, Comparison.
+
+// 1.Arithmetic Operators – (+, -, *, /, %)
+// 2.Assignment Operators – (=, +=, -=, *=, /=, %=)
+// 3.Relational (Comparison) Operators – (==, !=, >, <, >=, <=)
+// 4.Logical Operators – (&&, ||, !)
+// 5.Unary Operators - (++, --, +, -, !)
+// 6.Bitwise Operators – (&, |, ^, ~)
+// 7.Shift Operators – (<<, >>, >>>)
+// 8.Ternary Operator – (?:)
