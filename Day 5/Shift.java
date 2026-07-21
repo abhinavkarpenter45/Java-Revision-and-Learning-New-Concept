ca2921bd73dcf4d7166e7481c7c9e48106d9068c
Abhinav Karpenter <abhinavcarpenter0@gmail.com>
2026-07-21 22:11:59 +0530

    Day 5:Learn Concept of User Input and Operators

---
+++ Day 5/Shift.java
@@ -0,0 +1,16 @@
+public class Shift {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 10;
+        int c = - 10;
+
+        System.out.println(a << 1);
+        System.out.println(a << 2);
+
+        System.out.println(b >> 1);
+        System.out.println(b >> 2);
+
+        System.out.println(c >>> 1);
+    }
+}
